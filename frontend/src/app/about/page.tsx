bc1bbce0acf56d93b00660c238127f974216a7f9
solle458 <139096138+solle458@users.noreply.github.com>
2025-02-27 06:14:44 +0000

    about, workファイル追加

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,9 +12,6 @@ export default function Home() {
           height={38}
           priority
         />
-        <Link className='linkarrow' href='about' scroll={false}>
-            もっと知りたい / Explore further
-        </Link>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
