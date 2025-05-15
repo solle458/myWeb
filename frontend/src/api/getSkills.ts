09a6bbc21e4715762bf3736b29beacec1ba99180
solle458 <show04go@icloud.com>
2025-05-16 00:31:08 +0900

    feat : b : ヘッダーの変更

--- frontend/src/api/getSkills.ts
+++ frontend/src/api/getSkills.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export async function getSkills() {
     const url = (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api')+'/about/skills';
-    console.log("API URL:", url);
+    // console.log("API URL:", url);
     const res = await axios.get(url);
     if (res.status !== 200) {
         throw new Error('Failed to fetch data');
