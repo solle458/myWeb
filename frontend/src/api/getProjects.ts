c9f4f152c3baeac85a984b552cc1904d7b2dc643
solle458 <show04go@icloud.com>
2025-05-16 15:25:13 +0900

    feat : f : データの受け取り方法変更(getProject)

--- frontend/src/api/getProjects.ts
+++ frontend/src/api/getProjects.ts
@@ -1,4 +1,4 @@
-import { ProjectData, ProjectResponse } from '../types/projectTypes';
+import { ProjectData } from '../types/projectTypes';
 import axios from 'axios';
 
 // APIからプロジェクトデータを取得する関数
@@ -6,23 +6,31 @@ export async function getProjects(): Promise<ProjectData[]> {
   try {
     // APIリクエスト
     const url = (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api') + '/projects';
-    console.log("API URL:", url);
     const response = await axios.get(url);
-    console.log("API response:", response);
     // レスポンスが正常でない場合はエラーをスロー
     if (response.status < 200 || response.status >= 300) {
       throw new Error(`API error: ${response.status}`);
     }
-    
+    console.log('API response:', response.data);
     // レスポンスデータをパース
-    const result: ProjectResponse = response.data;
-    
-    // エラーチェック
-    if (!result.success) {
-      throw new Error(result.error || 'Unknown error');
-    }
-    
-    return result.data;
+    const results: ProjectData[] = response.data.map((projects: {
+      id: string;
+      title: string;
+      description: string[];
+      image: string;
+      technologies: string[];
+      url: string;
+      github: string;
+    }) => ({
+      id : projects.id,
+      title : projects.title,
+      description : projects.description,
+      image : projects.image,
+      technologies : projects.technologies,
+      url : projects.url,
+      github : projects.github
+    }));
+    return results;
   } catch (error) {
     console.error('Failed to fetch projects:', error);
     throw error;
