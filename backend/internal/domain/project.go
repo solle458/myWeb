a7a0ff3f41362933449a0c3c8dfa974f414925d0
solle458 <show04go@icloud.com>
2025-05-16 15:30:04 +0900

    ref : b : projectのデータ構造を変更

--- backend/internal/domain/project.go
+++ backend/internal/domain/project.go
@@ -2,10 +2,10 @@ package domain
 
 type Project struct {
 	ID          string   `json:"id,omitempty"`
-	Title       string   `json:"name"`
+	Title       string   `json:"title"`
 	Description string   `json:"description"`
-	Image       string   `json:"image_url"`
-	Technology  []string `json:"technology"`
+	Image       string   `json:"image"`
+	Technology  []string `json:"technologies"`
 	URL         string   `json:"url"`
 	Github      string   `json:"github"`
 	CreatedAt   string   `json:"created_at,omitempty"`
